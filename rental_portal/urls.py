3ede0501757bd38299ced1fb1fc31f194e73c2e3
Nimlord mwiti muriuki <nimlordmwiti88@gmail.com>
2025-12-17 00:42:36 +0300

    Add simple home page view

--- rental_portal/urls.py
+++ rental_portal/urls.py
@@ -1,10 +1,13 @@
-# rental_portal/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse # Add this import
+
+# A tiny view just for the home page
+def home_view(request):
+    return HttpResponse("<h1>Welcome to the Rental Portal API</h1><p>Visit <a href='/admin'>/admin</a> to login.</p>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # THIS LINE IS CRUCIAL: It directs all /api/ traffic to the tenant_api app
     path('api/', include('tenant_api.urls')), 
+    path('', home_view), # This fixes the 404 at the root URL
 ]
